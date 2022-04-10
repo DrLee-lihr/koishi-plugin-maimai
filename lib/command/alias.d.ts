41d6b9582b7795e502d15bc9e73eec9c475f193c
DrLee-lihr <812403lihr@sina.com>
2022-04-10 20:22:42 +0800

    ignore /lib

--- lib/command/alias.d.ts
+++
@@ -1,4 +0,0 @@
-import { Context } from "koishi";
-import { Config } from "..";
-import maimai_song_list from "../maimai_song_list";
-export default function (ctx: Context, config: Config, maisonglist: maimai_song_list): void;
