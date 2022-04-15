fabfc56785af392c6886fd45c6e85117842cd688
DrLee-lihr <812403lihr@sina.com>
2022-04-15 22:13:02 +0800

    new cmd:calc

--- src/command/calculate.ts
+++ src/command/calculate.ts
@@ -1,18 +1,9 @@
 import { Argv } from "koishi";
 import { Context } from "koishi";
 import { Config } from "..";
+import { difficulty } from "../maichart";
 import maimai_song_list from "../maimai_song_list";
-import { difficulty_trans_table } from "../mai_tool";
-
-
-
-declare module 'koishi' {
-  namespace Argv {
-    interface Domain {
-      difficulty: any
-    }
-  }
-}
+import { difficulty_trans_table, get_difficulty_id } from "../mai_tool";
 
 
 
@@ -20,21 +11,35 @@ declare module 'koishi' {
 
 export default function (ctx: Context, config: Config, maisonglist: maimai_song_list) {
 
-  Argv.createDomain('difficulty', (source) => {
-    switch(typeof source){
-      case 'number':{
 
-      }
-    }
-  })
 
   ctx.command('maimai')
-    .subcommand('calculate <id:number> <difficulty:string|number> <achievement>')
-    .action((_,id,diff,achieve)=>{
-      difficulty_trans_table[diff]
+    .subcommand('.calc <id:number> <difficulty:string> <achievement:number>')
+    .action((_, id, diff, achieve) => {
+      
+      let song = maisonglist.id(id)
+      let chart = song.charts[get_difficulty_id(diff)]
+      let notes = chart.object.notes
+
+
+      let max_combo = 0
+      notes.forEach((m) => max_combo += m)
+
+      let BREaK = song.is_sd ? notes[3] : notes[4] //break是保留字 xs
+      let touch = song.is_sd ? 0 : notes[3]
+      let tap_basic_num = notes[0] + notes[1] * 2 + notes[2] * 3 + (song.is_sd ? (notes[3] * 5) : (notes[3] + notes[4] * 5))
+
+      let available = 101 - achieve
+      return [
+        `${song.song_info_summary} ${chart.base_summary}`,
+        `物量：${max_combo}，基础计算物量：${tap_basic_num}`,
+        `1个 Tap Great -${(100 * 0.2 / tap_basic_num).toFixed(4)}%`,
+        `Break 50 落相当于${((0.5 / (2 * BREaK)) / (100 * 0.2 / tap_basic_num)).toFixed(4)}个 Tap Great (-${
+          (0.5 / (2 * BREaK)).toFixed(4)}%)`,
+        `${achieve}% 允许的最大 Tap Great 数量： ${(available / (100 * 0.2 / tap_basic_num)).toFixed(2)}`
+      ].join('\n')
+
     })
-    .alias('calc')
-
-
+    .shortcut('分数线',{fuzzy:true})
 
 }
