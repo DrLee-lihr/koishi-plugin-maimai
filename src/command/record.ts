d669b2cd43e71ee71f9ed3186ee9ec690a2c167b
DrLee-lihr <812403lihr@sina.com>
2022-05-04 15:22:17 +0800

    图不做了 直接发字得了

--- src/command/record.ts
+++ src/command/record.ts
@@ -8,7 +8,7 @@ import { fc, fs } from "./b40";
 import TextToSvg from 'text-to-svg'
 
 
-type record={
+type record = {
   achievements: number,
   fc: fc,
   fs: fs,
@@ -22,86 +22,79 @@ interface version_list {
   verlist: record[]
 }
 
+export async function get_record(ctx: Context, data: { qq: number } | { username: string }): Promise<version_list> {
+  data['version'] = Object.values(version_transform_table)
+  //console.log(Object.defineProperty(data,'version',Object.values(version_transform_table)))
+  return await
+    ctx.http.post('https://www.diving-fish.com/api/maimaidxprober/query/plate', data).catch(console.log)
+}
+
+async function get_version_record(ctx: Context, data: { qq: number } | { username: string }, version: string): Promise<version_list> {
+  data['version'] = [version]
+  return await ctx.http.post('https://www.diving-fish.com/api/maimaidxprober/query/plate', data).catch(console.log)
+}
+
 export default function record(ctx: Context, config: Config) {
-
-  async function get_record(data:{qq:number}|{username:string}):Promise<version_list> {
-    data['version']=Object.values(version_transform_table)
-    //console.log(Object.defineProperty(data,'version',Object.values(version_transform_table)))
-    return await 
-      ctx.http.post('https://www.diving-fish.com/api/maimaidxprober/query/plate',
-        data
-    ).catch(console.log)
-  }
-
-  let gener_img = (i: TextToSvg,text:string) => {
-    //FIXME: 文字不会换行
-    return sharp(Buffer.from(i.getSVG(text, {
-      fontSize: 50, anchor: 'left top', attributes: { fill: 'black' }
-    })))
-  }
 
   ctx.command('maimai')
     .subcommand('.record.level <level:string> [username:string] 获取对应标级的谱面的分数。')
-    .option('page', '-p <page:number> 当结果有多页时要输出的页码。')
+    .option('page', '-p <page:number> 当结果有多页时要输出的页码。', { fallback: 1 })
     .action(async ({ session, options }, level, username) => {
 
-      let data:{qq:number}|{username:string}
+      let data: { qq: number } | { username: string }
       if (username == undefined) {
-        if(session.platform!='onebot')return '请提供用户名。'
-        else data={qq:Number.parseInt(session.userId)}
+        if (session.platform != 'onebot') return '请提供用户名。'
+        else data = { qq: Number.parseInt(session.userId) }
       }
-      else data={username:username}
-      let result = await get_record(data)
+      else data = { username: username }
+      let result = await get_record(ctx, data)
       let list = result.verlist.filter(v => v.level == level)
-        .sort((a,b)=>b.achievements-a.achievements)
+        .sort((a, b) => b.achievements - a.achievements)
 
-      let text = page_split<record>(
-        list, config, options.page, 
-        `${username??session.username} 的 ${level} 分数列表：`,
+      return page_split<record>(
+        list, config, options.page,
+        `${username ?? session.username} 的 ${level} 分数列表：`,
         v => `${v.achievements}% ` +
-            maisonglist.id(v.id).charts[v.level_index].chart_summary_with_base
-        )
-      
-      
-  
-      return s.image(await gener_img(tts,text).toBuffer())
-
+          maisonglist.id(v.id).charts[v.level_index].chart_summary_with_base
+      )
     })
-    .shortcut(/^((?:[1-9])|(?:1[0-5])\+?)分数列表(?: ([0-9]{1,}))?$/, 
+    .shortcut(/^((?:[1-9])|(?:1[0-5])\+?)分数列表(?: ([0-9]{1,}))?$/,
       { args: ['$1'], options: { page: '$2' } })
 
   ctx.command('maimai')
     .subcommand('.record.base <base:number> [username:string] 获取对应定数的谱面的分数。')
-    .option('page', '-p <page:number> 当结果有多页时要输出的页码。')
+    .option('page', '-p <page:number> 当结果有多页时要输出的页码。', { fallback: 1 })
     .action(async ({ session, options }, base, username) => {
 
-      let data:{qq:number}|{username:string}
+      let data: { qq: number } | { username: string }
       if (username == undefined) {
-        if(session.platform!='onebot')return '请提供用户名。'
-        else data={qq:Number.parseInt(session.userId)}
+        if (session.platform != 'onebot') return '请提供用户名。'
+        else data = { qq: Number.parseInt(session.userId) }
       }
-      else data={username:username}
-      let result = await get_record(data)
+      else data = { username: username }
+      let result = await get_record(ctx, data)
 
-      let res:string[]=[]
+      let res: string[] = []
 
-      let list = result.verlist.sort((a,b)=>b.achievements-a.achievements).forEach(
-        (v)=>{
-          let song=maisonglist.id(v.id)
-          if(song.charts[v.level_index].ds==base)
+      result.verlist.sort((a, b) => b.achievements - a.achievements).forEach(
+        (v) => {
+          let song = maisonglist.id(v.id)
+          if (song.charts[v.level_index].ds == base)
             res.push(
               `${v.achievements}% ` +
               maisonglist.id(v.id).charts[v.level_index].chart_summary_with_base
             )
         }
       )
+      return page_split(
+        res, config, options.page, `${username ?? session.username} 的 ${base} 分数列表：`
+      )
+    }).shortcut(/^((?:[1-9])|(?:1[0-5]).[0-9])分数列表(?: ([0-9]{1,}))?$/,
+      { args: ['$1'], options: { page: '$2' } })
 
+  ctx.command('maimai')
+    .subcommand('.record.song <identifier:string> <difficulty:number> [username:string] 获取谱面的达成分数。')
+    .action((identifier,difficulty,username)=>{
 
-      let text =  page_split(
-        res, config, options.page, `${username??session.username} 的 ${base} 分数列表：`
-        )
-
-      return s.image(await gener_img(tts,text).toBuffer())
     })
-
 }
